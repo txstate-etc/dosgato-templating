12ef5e6237d23cbc3bb98352196394d48884903c
Nick Wing <nwing@txstate.edu>
2022-06-09 10:53:55 -0500

    better message passing of events on edit and new bars

--- src/editbar.ts
+++ src/editbar.ts
@@ -10,11 +10,11 @@ export function editBar (path: string, opts: EditBarOpts & { label: string }) {
   if (!opts.editMode) return ''
   const id = randomid()
   return `
-<div class="dg-edit-bar ${opts.extraClass ?? ''}" data-path="${htmlEncode(path)}">
+<div class="dg-edit-bar ${opts.extraClass ?? ''}" data-path="${htmlEncode(path)}" draggable="true" ondragstart="window.dgEditing.drag(event)" ondragover="window.dgEditing.over(event)" ondragend="window.dgEditing.drop(event)">
   <span id="${id}" class="dg-edit-bar-label">${htmlEncode(opts.label)}</span>
-  <button onClick="window.dgEditing.edit" aria-describedby="${id}">Edit</button>
-  <button onClick="window.dgEditing.move" aria-describedby="${id}">Move</button>
-  <button onClick="window.dgEditing.del" aria-describedby="${id}">Trash</button>
+  <button onclick="window.dgEditing.edit(event)" aria-describedby="${id}">Edit</button>
+  <button onclick="window.dgEditing.move(event)" aria-describedby="${id}">Move</button>
+  <button onclick="window.dgEditing.del(event)" aria-describedby="${id}">Trash</button>
 </div>
   `.trim()
 }
@@ -22,8 +22,8 @@ export function editBar (path: string, opts: EditBarOpts & { label: string }) {
 export function newBar (path: string, opts: EditBarOpts & { label: string }) {
   if (!opts.editMode) return ''
   return `
-<div class="dg-new-bar ${opts.extraClass ?? ''}" data-path="${htmlEncode(path)}">
-  <button>${htmlEncode(opts.label)}</button>
+<div role="button" onclick="window.dgEditing.create(event)" class="dg-new-bar ${opts.extraClass ?? ''}" data-path="${htmlEncode(path)}">
+  ${htmlEncode(opts.label)}
 </div>
   `.trim()
 }

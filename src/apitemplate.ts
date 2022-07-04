d12162bbd0d333e7bd721be29d719428a75b32ae
Nick Wing <nwing@txstate.edu>
2022-07-04 11:07:39 -0500

    make validation feedback type optional (default is 'error'), release 0.0.28

--- src/apitemplate.ts
+++ src/apitemplate.ts
@@ -11,7 +11,7 @@ export enum ValidationMessageType {
 }
 
 export interface ValidationFeedback {
-  type: `${ValidationMessageType}`
+  type?: `${ValidationMessageType}`
   path?: string
   message: string
 }

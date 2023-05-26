c1ad7d68e1725682284c984db9ce828ab411989b
Drew Pilling (aap64) <Drew.Pilling@TxState.edu>
2023-05-26 15:45:33 -0500

    Updated the uiInteractionsLogger to accept an optional config for passing things like the apiBase to it from the environmentConfig.

--- src/uitemplate.ts
+++ src/uitemplate.ts
@@ -153,21 +153,25 @@ export interface TracingInterface {
 
 export interface BaseEvent {
   /** The larger UI area the user is interacting with that the event is emitted from.
-   * @example 'ActionPanel', 'PageEditor', 'ComponetDialog' */
-  eventType: string
+   * @example 'ActionPanel', 'PageEditor', 'ComponentDialog' */
+  eventType: string // How about renaming to `emitterContext`?
 
   /** The specific action the user took. Typically the label for the element that emits
-   * the event for instance.
+   * the event.
    * @example 'Add Page', 'Edit Page', 'Preview', 'Cancel Preview' */
   action: string
 
   /** Additional data points specific to a particular event type's context. These should
    * be significant enough to understanding the event to merrit adding additional columns
    * in tools like elastic-search.
-   * @warning This is NOT a catch-all property. */
+   * @warning This is NOT a catch-all property.
+   * @example { hiddenLabel: action.hiddenLabel } // The aria label for an action element. */
   additionalProperties?: Record<string, string | undefined>
 }
 
+/** Events triggered by user interactions with interface elements in DosGato. This interface
+ * is intended to provide a common model for succinctly expressing the contextually important
+ * properties of these events to loggers that can be pointed to analytics and metrics services. */
 export interface UserEvent extends BaseEvent {
   /** The page, screen, or dialog the user is looking at in which the associated event emitter is
    * in context to.
@@ -175,8 +179,8 @@ export interface UserEvent extends BaseEvent {
   screen: string
 
   /** The target the emitted event is to trigger actions on.
-   * Each page/screen, or dialog, needs to set their target for what events in it are targeted to act on in
-   * in its context.
+   * Each page/screen, or dialog, needs to set their target for what events in it are targeted
+   * to act on in in its context.
    *
    * For example: The page in the page tree of the Pages screen that ActionPanel actions,
    * such as edit or preview, will act on.
@@ -250,5 +254,5 @@ export interface UIConfig {
    * Useful for defining how to log form submissions, interaction clicks, page edits, or state
    * changes of different interfaces. Can be directed to separate endpoint for APM logging as
    * long as that POST is also non-awaited. */
-  uiInteractionsLogger?: (info: UserEvent) => void
+  uiInteractionsLogger?: (info: UserEvent, config?: any) => void
 }
